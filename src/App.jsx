45eeda7a23408a13d9b95bc39e0a4b967e0fced3
Reeya <reeya.baidya@gmail.com>
2022-11-16 21:10:09 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from './components/header/Header'
+const App = () => {
+  return (
+    <Header/>
+  )
+}
+
+export default App
---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './experience.css'
+const Experirnce = () => {
+  return (
+    <div>Experirnce</div>
+  )
+}
+
+export default Experirnce
