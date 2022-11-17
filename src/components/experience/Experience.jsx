c83b2a2af6eb51f4442567663245dee99fd4de13
Reeya <reeya.baidya@gmail.com>
2022-11-17 13:40:15 +0545

    nav bar done!

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './experience.css'
 const Experirnce = () => {
   return (
-    <div>Experirnce</div>
+    <section id='experience'>Experience</section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {AiOutlineBook} from 'react-icons/ai'
+import {AiOutlinePhone} from 'react-icons/ai'
+import { useState} from 'react' /* for nav bar active state*/
+
 const Nav = () => {
+  const [activeNav, setActiveNav]=useState('#');
   return (
-    <div>Nav</div>
+    <nav> 
+      {/* if avtiveNav is this then give active class 'active' else give ''  */}
+      <a href='#' onClick={() =>setActiveNav('#')} className={activeNav==='#' ? 'active': ''}><AiOutlineHome/></a>
+      <a href='#about' onClick={() =>setActiveNav('#about')}  className={activeNav==='#about' ? 'active': ''}><AiOutlineUser/></a>
+      <a href='#experience' onClick={() =>setActiveNav('#experience')}  className={activeNav==='#experience' ? 'active': ''}><AiOutlineBook/></a>
+      <a href='#contact' onClick={() =>setActiveNav('#contact')}  className={activeNav==='#contact' ? 'active': ''}><AiOutlinePhone/></a>
+    </nav>
   )
 }
 
