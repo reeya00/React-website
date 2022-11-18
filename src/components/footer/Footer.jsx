8007885ead6fc4b599340f9790a57719fb7d9120
Reeya <reeya.baidya@gmail.com>
2022-11-18 14:42:49 +0545

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {BsInstagram} from 'react-icons/bs'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer_logo'>Reeya Baidya</a>
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer_socials'>
+        <a href='https://facebook.com'><FaFacebookF/></a>
+        <a href='https://instagram.com'><BsInstagram/></a>
+      </div>
+
+      <div className='footer_copyright'>
+        <small>&copy; REEYA BAIDYA. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
