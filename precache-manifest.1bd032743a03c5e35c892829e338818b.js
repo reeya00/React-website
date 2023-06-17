2c98185cd0706d63b23c30b7548486bef75a60f6
Reeya <reeya.baidya@gmail.com>
2023-06-17 12:59:20 +0545

    Updates

---
+++ precache-manifest.1bd032743a03c5e35c892829e338818b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "07b61ad1bd53f4497c8e",
+    "url": "/React-website/static/css/main.37b1c697.chunk.css"
+  },
+  {
+    "revision": "07b61ad1bd53f4497c8e",
+    "url": "/React-website/static/js/main.92b458e2.chunk.js"
+  },
+  {
+    "revision": "8fd310b3e1215e18860d",
+    "url": "/React-website/static/js/runtime~main.d99295d9.js"
+  },
+  {
+    "revision": "f35b286e2eaa155cab4b",
+    "url": "/React-website/static/css/2.9a701826.chunk.css"
+  },
+  {
+    "revision": "f35b286e2eaa155cab4b",
+    "url": "/React-website/static/js/2.c1322033.chunk.js"
+  },
+  {
+    "revision": "97727638ee70e5f93d354a0cd740c0e8",
+    "url": "/React-website/static/media/Removal-544.97727638.png"
+  },
+  {
+    "revision": "d3ec82b6b374ed015820e54fd78ff77c",
+    "url": "/React-website/static/media/CV_Of_Reeya.d3ec82b6.pdf"
+  },
+  {
+    "revision": "0e81bd1d855c262ade9ae00e96c652c8",
+    "url": "/React-website/static/media/pic2.0e81bd1d.jpeg"
+  },
+  {
+    "revision": "690094e28d4b8017db5a8282cdcb5983",
+    "url": "/React-website/static/media/portfolio1.690094e2.jpg"
+  },
+  {
+    "revision": "1752abe46b7062ffaabcc820850fe20c",
+    "url": "/React-website/static/media/portfolio2.1752abe4.jpg"
+  },
+  {
+    "revision": "d52a144287dbd7eb8e95765e35026e3c",
+    "url": "/React-website/static/media/portfolio3.d52a1442.jpg"
+  },
+  {
+    "revision": "d92fe2c51ccfbcec65d839b207d89e18",
+    "url": "/React-website/static/media/portfolio4.d92fe2c5.jpg"
+  },
+  {
+    "revision": "4f032bb6255fbcbe50d943f89ed79701",
+    "url": "/React-website/static/media/portfolio5.4f032bb6.jpg"
+  },
+  {
+    "revision": "a446eead7128119fbc9a76130cb2e68b",
+    "url": "/React-website/static/media/portfolio6.a446eead.jpg"
+  },
+  {
+    "revision": "74eb9be7d1995beea61feab505b5daaf",
+    "url": "/React-website/static/media/portfolio7.74eb9be7.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/React-website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/React-website/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/React-website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/React-website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "7d61e290e5f47b02dce07141f294afb9",
+    "url": "/React-website/index.html"
+  }
+];
