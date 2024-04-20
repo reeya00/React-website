140c149618a2c6cc0bd26496197e3832d641dcec
Reeya <84576848+reeya00@users.noreply.github.com>
2024-04-20 22:54:37 +0545

    link portfolio changed

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
     <div className='cta'>
         {/* download pdf */}
         {/* <a href={CV}download className='btn'>Download</a> */}
-        <a href="https://bit.ly/reeyabaidya_portfolio" className='btn'>View Portfolio</a>
+        <a href="https://www.figma.com/proto/jenWdDcdNZy4GsuCXWTSY7/Reeya-Portfolio?page-id=809%3A1633&type=design&node-id=809-2410&viewport=4440%2C-2451%2C0.33&t=LmbzRO59hla9lH8c-1&scaling=contain&starting-point-node-id=809%3A2410&mode=design" className='btn'>View Portfolio</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
